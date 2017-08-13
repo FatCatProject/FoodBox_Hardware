1a5a426da2679bed72fe2088066975933726af0b
SheyDenis <denis.sheyer@gmail.com>
2017-08-13 14:57:22 +0300

    Added a fake RPi package for Windows testing and development.

---
+++ RPi/GPIO.py
@@ -0,0 +1,27 @@
+BOARD = 1
+OUT = 1
+IN = 1
+
+
+def setmode(a):
+	print(a)
+
+
+def setup(a, b):
+	print(a)
+
+
+def output(a, b):
+	print(a)
+
+
+def cleanup():
+	print('a')
+
+
+def setmode(a):
+	print(a)
+
+
+def setwarnings(flag):
+	print('False')
