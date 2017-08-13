d7c8a6e136c4faae247041f49c5b26aecb0c6497
SheyDenis <denis.sheyer@gmail.com>
2017-08-14 01:36:31 +0300

    Minor cleanup and corrections.

--- Packages/LM393/proximity.py
+++ Packages/LM393/proximity.py
@@ -1,9 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
-
 # while True:
 # 	print(1-(GPIO.input(17))) <-- by default 0=sensor blocked , 1 sensor opened
 
@@ -13,9 +10,14 @@ class Proximity:
 
 	def __init__(self, pin_num=17):
 		self.__pin_num = pin_num
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.__pin_num, GPIO.IN)
+
+	def __del__(self):
+		GPIO.cleanup()
 
 	def is_blocked(self):
-		if((1-(GPIO.input(self.__pin_num)))==0):
+		if (1 - GPIO.input(self.__pin_num)) == 0:
 			return False
 		else:
 			return True
