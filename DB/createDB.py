b2799d79b09471e657ae7a6064117399e249b3dd
Shabnata <mer.natalie@gmail.com>
2017-08-20 15:33:48 +0300

    added new functions to foodboxDB

--- DB/createDB.py
+++ DB/createDB.py
@@ -3,45 +3,50 @@ import os.path
 
 
 def create_foodboxDB():
-	# Create new file and connect to it
-	conn = sqlite3.connect('foodboxDB.db')
+	"""
+	Create new DB file and connect to it
+	Check if the DB was created successfully
+	If not throw exception
+	"""
+	try:
+		conn = sqlite3.connect('foodboxDB.db')
+	except sqlite3.OperationalError:
+		pass
+	finally:
+		# TODO logic
+		print('Eror creating DB')
 
-	# Check if file was created successfully
-	if os.path.exists('foodboxDB.db') == False:
-		# TODO  Create Exeption for error on creating a DB file instead of printing it
-		print('Error creating DB file')
-	else:
-		c = conn.cursor()
-		c.execute('CREATE TABLE IF NOT EXISTS system_settings('
-				  'key_name TEXT NOT NULL , '
-				  'value_text TEXT, '
-				  'PRIMARY KEY(`key_name`) );')
+	c = conn.cursor()
+	c.execute('CREATE TABLE IF NOT EXISTS system_settings('
+			  'key_name TEXT NOT NULL , '
+			  'value_text TEXT, '
+			  'PRIMARY KEY(`key_name`) );')
 
-		c.execute('CREATE TABLE IF NOT EXISTS system_logs('
-				  'card_id TEXT, '
-				  'time_stamp TEXT NOT NULL,'
-				  'message TEXT, '
-				  'message_type TEXT NOT NULL,'
-				  'severity INTEGER NOT NULL,'
-				  'FOREIGN KEY(`card_id`) REFERENCES `cards`, '
-				  'FOREIGN KEY(`message_type`) REFERENCES `message_types` );')
+	c.execute('CREATE TABLE IF NOT EXISTS system_logs('
+			  'card_id TEXT, '
+			  'time_stamp TEXT NOT NULL,'
+			  'message TEXT, '
+			  'message_type TEXT NOT NULL,'
+			  'severity INTEGER NOT NULL,'
+			  'FOREIGN KEY(`card_id`) REFERENCES `cards`, '
+			  'FOREIGN KEY(`message_type`) REFERENCES `message_types` );')
 
-		c.execute('CREATE TABLE IF NOT EXISTS feeding_logs('
-				  'feeding_id INTEGER NOT NULL ,'
-				  'card_id TEXT NOT NULL,'
-				  'open_time TEXT NOT NULL,'
-				  'close_time TEXT NOT NULL,'
-				  'starting_weight NUMERIC NOT NULL,'
-				  'ending_weight NUMERIC NOT NULL,'
-				  'synced INTEGER NOT NULL,'
-				  'PRIMARY KEY(`feeding_id`), '
-				  'FOREIGN KEY(`card_id`) REFERENCES `cards` );')
+	c.execute('CREATE TABLE IF NOT EXISTS feeding_logs('
+			  'feeding_id INTEGER NOT NULL ,'
+			  'card_id TEXT NOT NULL,'
+			  'open_time TEXT NOT NULL,'
+			  'close_time TEXT NOT NULL,'
+			  'starting_weight NUMERIC NOT NULL,'
+			  'ending_weight NUMERIC NOT NULL,'
+			  'synced INTEGER NOT NULL,'
+			  'PRIMARY KEY(`feeding_id`), '
+			  'FOREIGN KEY(`card_id`) REFERENCES `cards` );')
 
-		c.execute('CREATE TABLE IF NOT EXISTS cards('
-				  '`card_id` TEXT NOT NULL , '
-				  '`active` INTEGER NOT NULL,'
-				  'PRIMARY KEY(`card_id`) );')
+	c.execute('CREATE TABLE IF NOT EXISTS cards('
+			  '`card_id` TEXT NOT NULL , '
+			  '`active` INTEGER NOT NULL,'
+			  'PRIMARY KEY(`card_id`) );')
 
-		conn.commit()
-		c.close()
-		conn.close()
+	conn.commit()
+	c.close()
+	conn.close()
