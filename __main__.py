0eaffa08997a63cd29b34247fdab09526ff9b1f5
Denis Sh <denis.sheyer@gmail.com>
2017-08-29 12:31:38 +0300

    Add GPIO cleanup to when the program ends.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,15 @@
 from foodbox.foodbox import FoodBox
+from RPi.GPIO import GPIO
+
+
 def main():
 	box = FoodBox()
-	box.start_mainloop()
+	try:
+		box.start_mainloop()
+	except Exception as e:
+		raise e
+	finally:
+		GPIO.cleanup()
 
 if __name__ == "__main__":
 	main()
