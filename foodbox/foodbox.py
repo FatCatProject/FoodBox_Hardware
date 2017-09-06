bb203fb80d0c3ca665c79fad7386c691415ef198
Shabnata <mer.natalie@gmail.com>
2017-09-06 17:18:18 +0300

    added prints for weight and feeding_id

--- foodbox/foodbox.py
+++ foodbox/foodbox.py
@@ -272,6 +272,7 @@ class FoodBox:
 			self.write_system_log(syslog)
 			open_time = time.localtime()
 			start_weight = self.__scale.get_units()
+			print("Starting weight is: ",start_weight)
 			self.open_lid()
 			time.sleep(5)
 			if card.get_name() == "ADMIN":
@@ -291,9 +292,12 @@ class FoodBox:
 			self.close_lid()
 			close_time = time.localtime()
 			end_weight = self.__scale.get_units()
+			print("End weight is: ", end_weight)
 			feedinglog = FeedingLog(card=card, open_time=open_time, close_time=close_time, start_weight=start_weight,
 				end_weight=end_weight)
+			print("The feeding id is: ", feedinglog.get_id())
 			self.write_feeding_log(feedinglog)
+			del feedinglog
 			if self.__sync_on_change:
 				sync_uid, sync_success = self.sync_with_brainbox()
 				if sync_success:
