24736cff8272c5d275054e7af1fe23e7d853b1b6
SheyDenis <denis.sheyer@gmail.com>
2017-10-23 16:04:41 +0300

    Closes #67

--- DB/createDB.py
+++ DB/createDB.py
@@ -12,43 +12,49 @@ def create_foodboxDB():
 	print("create_foodboxDB() started.")  # Debug message
 
 	try:
-		conn = sqlite3.connect('foodboxDB.db')
+		conn = sqlite3.connect("foodboxDB.db")
+
+		query_pragma = [
+			"PRAGMA foreign_keys = ON;"
+		]
+		query_system_settings = [
+			"CREATE TABLE IF NOT EXISTS system_settings(",
+			"key_name TEXT NOT NULL, value_text TEXT, PRIMARY KEY('key_name')",
+			");"
+		]
+		query_system_logs = [
+			"CREATE TABLE IF NOT EXISTS system_logs(",
+			"card_id TEXT, time_stamp NUMERIC NOT NULL, message TEXT,",
+			"message_type TEXT NOT NULL, severity INTEGER NOT NULL",
+			");"
+		]
+		query_feeding_logs = [
+			"CREATE TABLE IF NOT EXISTS feeding_logs(",
+			"feeding_id TEXT NOT NULL, card_id TEXT NOT NULL,",
+			"open_time NUMERIC NOT NULL, close_time NUMERIC NOT NULL,",
+			"start_weight NUMERIC NOT NULL, end_weight NUMERIC NOT NULL,",
+			"synced INTEGER NOT NULL, PRIMARY KEY('feeding_id'),",
+			"FOREIGN KEY('card_id') REFERENCES 'cards'",
+			");"
+		]
+		query_cards = [
+			"CREATE TABLE IF NOT EXISTS cards(",
+			"card_id TEXT NOT NULL, active INTEGER NOT NULL, card_name TEXT,",
+			"PRIMARY KEY('card_id')",
+			");"
+		]
 
 		c = conn.cursor()
-		c.execute("PRAGMA foreign_keys = ON")
-		c.execute('CREATE TABLE IF NOT EXISTS system_settings('
-				  'key_name TEXT NOT NULL , '
-				  'value_text TEXT, '
-				  'PRIMARY KEY(`key_name`) );')
-
-		c.execute('CREATE TABLE IF NOT EXISTS system_logs('
-				  'card_id TEXT, '
-				  'time_stamp NUMERIC NOT NULL,'
-				  'message TEXT, '
-				  'message_type TEXT NOT NULL,'
-				  'severity INTEGER NOT NULL);')
-
-		c.execute('CREATE TABLE IF NOT EXISTS feeding_logs('
-				  'feeding_id TEXT NOT NULL ,'
-				  'card_id TEXT NOT NULL,'
-				  'open_time NUMERIC NOT NULL,'
-				  'close_time NUMERIC NOT NULL,'
-				  'start_weight NUMERIC NOT NULL,'
-				  'end_weight NUMERIC NOT NULL,'
-				  'synced INTEGER NOT NULL,'
-				  'PRIMARY KEY(`feeding_id`), '
-				  'FOREIGN KEY(`card_id`) REFERENCES `cards` );')
-
-		c.execute('CREATE TABLE IF NOT EXISTS cards('
-				  '`card_id` TEXT NOT NULL , '
-				  '`active` INTEGER NOT NULL,'
-				  '`card_name` TEXT, '
-				  'PRIMARY KEY(`card_id`) );')
+		c.execute(str.join(" ", query_pragma))
+		c.execute(str.join(" ", query_system_settings))
+		c.execute(str.join(" ", query_system_logs))
+		c.execute(str.join(" ", query_feeding_logs))
+		c.execute(str.join(" ", query_cards))
 
 		conn.commit()
 		c.close()
 		conn.close()
-	except sqlite3.OperationalError:
-		print('Error creating DB')
+	except sqlite3.OperationalError as e:
+		print("Error creating DB - {}".format(e.args))
 
 	print("create_foodboxDB() ended.")  # Debug message
