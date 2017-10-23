6f761e125f06aedad9fed88d999105733b7072b2
SheyDenis <denis.sheyer@gmail.com>
2017-10-23 14:21:35 +0300

    Closes #68

--- DB/createDB.py
+++ DB/createDB.py
@@ -13,46 +13,42 @@ def create_foodboxDB():
 
 	try:
 		conn = sqlite3.connect('foodboxDB.db')
+
+		c = conn.cursor()
+		c.execute("PRAGMA foreign_keys = ON")
+		c.execute('CREATE TABLE IF NOT EXISTS system_settings('
+				  'key_name TEXT NOT NULL , '
+				  'value_text TEXT, '
+				  'PRIMARY KEY(`key_name`) );')
+
+		c.execute('CREATE TABLE IF NOT EXISTS system_logs('
+				  'card_id TEXT, '
+				  'time_stamp NUMERIC NOT NULL,'
+				  'message TEXT, '
+				  'message_type TEXT NOT NULL,'
+				  'severity INTEGER NOT NULL);')
+
+		c.execute('CREATE TABLE IF NOT EXISTS feeding_logs('
+				  'feeding_id TEXT NOT NULL ,'
+				  'card_id TEXT NOT NULL,'
+				  'open_time NUMERIC NOT NULL,'
+				  'close_time NUMERIC NOT NULL,'
+				  'start_weight NUMERIC NOT NULL,'
+				  'end_weight NUMERIC NOT NULL,'
+				  'synced INTEGER NOT NULL,'
+				  'PRIMARY KEY(`feeding_id`), '
+				  'FOREIGN KEY(`card_id`) REFERENCES `cards` );')
+
+		c.execute('CREATE TABLE IF NOT EXISTS cards('
+				  '`card_id` TEXT NOT NULL , '
+				  '`active` INTEGER NOT NULL,'
+				  '`card_name` TEXT, '
+				  'PRIMARY KEY(`card_id`) );')
+
+		conn.commit()
+		c.close()
+		conn.close()
 	except sqlite3.OperationalError:
-		pass
-	finally:
-		# TODO logic
-		pass
-	# print('Error creating DB')
-
-	c = conn.cursor()
-	c.execute("PRAGMA foreign_keys = ON")
-	c.execute('CREATE TABLE IF NOT EXISTS system_settings('
-			  'key_name TEXT NOT NULL , '
-			  'value_text TEXT, '
-			  'PRIMARY KEY(`key_name`) );')
-
-	c.execute('CREATE TABLE IF NOT EXISTS system_logs('
-			  'card_id TEXT, '
-			  'time_stamp NUMERIC NOT NULL,'
-			  'message TEXT, '
-			  'message_type TEXT NOT NULL,'
-			  'severity INTEGER NOT NULL);')
-
-	c.execute('CREATE TABLE IF NOT EXISTS feeding_logs('
-			  'feeding_id TEXT NOT NULL ,'
-			  'card_id TEXT NOT NULL,'
-			  'open_time NUMERIC NOT NULL,'
-			  'close_time NUMERIC NOT NULL,'
-			  'start_weight NUMERIC NOT NULL,'
-			  'end_weight NUMERIC NOT NULL,'
-			  'synced INTEGER NOT NULL,'
-			  'PRIMARY KEY(`feeding_id`), '
-			  'FOREIGN KEY(`card_id`) REFERENCES `cards` );')
-
-	c.execute('CREATE TABLE IF NOT EXISTS cards('
-			  '`card_id` TEXT NOT NULL , '
-			  '`active` INTEGER NOT NULL,'
-			  '`card_name` TEXT, '
-			  'PRIMARY KEY(`card_id`) );')
-
-	conn.commit()
-	c.close()
-	conn.close()
+		print('Error creating DB')
 
 	print("create_foodboxDB() ended.")  # Debug message
